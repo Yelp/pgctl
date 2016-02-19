b7a9f42bf777689344a93a16e1dedf350b1ff352
Anthony Sottile <asottile@umich.edu>
2016-02-18 18:53:14 -0800

    object is a 'class' not a 'type' in python3

--- tests/unit/functions.py
+++ tests/unit/functions.py
@@ -4,6 +4,7 @@ from __future__ import absolute_import
 from __future__ import print_function
 from __future__ import unicode_literals
 
+import six
 from frozendict import frozendict
 from testfixtures import ShouldRaise
 from testing.norm import norm_trailing_whitespace_json
@@ -49,7 +50,8 @@ class DescribeJSONEncoder(object):
 }'''
 
     def it_encodes_other(self):
-        with ShouldRaise(TypeError("<type 'object'> is not JSON serializable")):
+        msg = 'type' if six.PY2 else 'class'
+        with ShouldRaise(TypeError("<{} 'object'> is not JSON serializable".format(msg))):
             JSONEncoder(sort_keys=True, indent=4).encode(object)
 
 
