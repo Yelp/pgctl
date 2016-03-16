53506579f3f16b6460505811666f5a7e3eec24db
Buck Golemon <buck@yelp.com>
2016-03-16 16:03:11 -0700

    simplify venv

--- .activate.sh
+++ .activate.sh
@@ -1,6 +1,6 @@
 #!/not/executable/bash
 # TODO: make aactivator cd to the top directory before sourcing
 # TODO: use venv-update to make this much more lightweight
-make devenv
+make venv
 export TOP=$PWD
-source .tox/devenv/bin/activate
+source venv/bin/activate
