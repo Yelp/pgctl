b44575f16d79131cffcc5e33ab8acf5f7233b22e
Chris Kuehl <ckuehl@yelp.com>
2016-07-15 13:55:52 -0700

    Release v1.7.0

--- pgctl/__init__.py
+++ pgctl/__init__.py
@@ -2,4 +2,4 @@ from __future__ import absolute_import
 from __future__ import unicode_literals
 
 # NOTE: this file is imported by setup.py, so cannot have any dependencies or side-effect.
-__version__ = '1.6.3'
+__version__ = '1.7.0'
