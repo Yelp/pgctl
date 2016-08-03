b6c54b6de7e314aa7572edf02d109774ba85bb92
Chris Kuehl <ckuehl@yelp.com>
2016-08-03 15:31:10 -0700

    Stop doing crazy things in .activate.sh

--- .activate.sh
+++ .activate.sh
@@ -1,6 +1,1 @@
-#!/not/executable/bash
-# TODO: make aactivator cd to the top directory before sourcing
-# TODO: use venv-update to make this much more lightweight
-make venv
-export TOP=$PWD
-source venv/bin/activate
+venv/bin/activate
