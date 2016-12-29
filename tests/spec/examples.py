8ec6e361ecea4c2ef1469a519d58105c21f53e36
Ya-Lin Huang <yalinh@yelp.com>
2016-12-29 14:13:57 -0800

    Disable pylint max-module-lines in examples.py

--- tests/spec/examples.py
+++ tests/spec/examples.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# pylint:disable=no-self-use, unused-argument
+# pylint:disable=no-self-use, unused-argument, too-many-lines
 from __future__ import absolute_import
 from __future__ import print_function
 from __future__ import unicode_literals
