d47103bf4d2f8c79f4d9995a3fb5e235c38c1e64
Noah Gold <ngold@yelp.com>
2017-01-30 10:41:51 -0800

    This is v2.5.4

--- pgctl/__init__.py
+++ pgctl/__init__.py
@@ -2,4 +2,4 @@ from __future__ import absolute_import
 from __future__ import unicode_literals
 
 # NOTE: this file is imported by setup.py, so cannot have any dependencies or side-effect.
-__version__ = '2.5.3'
+__version__ = '2.5.4'
