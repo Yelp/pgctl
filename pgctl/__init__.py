d6931e4ccb9b37b4a8059c9e02a30a7cc0c31058
Noah Gold <ngold@yelp.com>
2017-03-07 17:23:36 -0800

    This is v2.6.0

--- pgctl/__init__.py
+++ pgctl/__init__.py
@@ -2,4 +2,4 @@ from __future__ import absolute_import
 from __future__ import unicode_literals
 
 # NOTE: this file is imported by setup.py, so cannot have any dependencies or side-effect.
-__version__ = '2.5.5'
+__version__ = '2.6.0'
