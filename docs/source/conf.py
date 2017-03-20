0320ac84e8dcd67b42abd1b4f8555c8e5b9340b5
Chris Kuehl <ckuehl@yelp.com>
2017-03-20 10:38:39 -0700

    update pre-commit

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -54,9 +54,9 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'pgctl'
-copyright = u'2015 Yelp Inc.'
-author = u'Buck Evan'
+project = 'pgctl'
+copyright = '2015 Yelp Inc.'
+author = 'Buck Evan'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -232,8 +232,8 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'pgctl.tex', u'pgctl Documentation',
-     u'Buck Evan', 'manual'),
+    (master_doc, 'pgctl.tex', 'pgctl Documentation',
+     'Buck Evan', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -262,7 +262,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'pgctl', u'pgctl Documentation',
+    (master_doc, 'pgctl', 'pgctl Documentation',
      [author], 1)
 ]
 
@@ -276,7 +276,7 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    (master_doc, 'pgctl', u'pgctl Documentation',
+    (master_doc, 'pgctl', 'pgctl Documentation',
      author, 'pgctl', 'One line description of project.',
      'Miscellaneous'),
 ]
