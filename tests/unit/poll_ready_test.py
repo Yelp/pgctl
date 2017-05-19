96d9ab3bf5ff9e86f982aa7f925cc20876c10a98
Noah Gold <ngold@yelp.com>
2017-05-19 15:06:51 -0700

    Added a test to fix coverage flakes.

--- tests/unit/poll_ready_test.py
+++ tests/unit/poll_ready_test.py
@@ -48,3 +48,27 @@ class DescribeGetVal(object):
         result = poll_ready.getval('does not exist', 'SVWAIT', '2')
         assert isinstance(result, float)
         assert result == 2.0
+
+
+def test_main_coverage_hack():
+    """For some reason, coverage flakes on the first branch of poll-ready's
+    main function. This test executes the lines in that branch to avoid
+    test failures that are already ignored by the maintainers.
+
+    We suspect the underlying coverage problem is related to the exec call and
+    coverage not getting collecting data before the new image executable image
+    is loaded, but have not been able to fix it.
+    """
+    class CoverageHackException(Exception):
+        pass
+
+    with mock.patch.object(
+        poll_ready,
+        'exec_',
+        autospec=True,
+    ) as mock_exec, mock.patch.dict(
+            os.environ,
+            {'PGCTL_DEBUG': 'true'},
+    ), pytest.raises(CoverageHackException):
+        mock_exec.side_effect = CoverageHackException
+        poll_ready.main()
